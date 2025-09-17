b9b078199016197711f228697fd4944b43c269b1
northwww <egorsavin943@gmail.com>
2025-09-17 22:09:37 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+const paragraphs = document.querySelectorAll("p");
+console.log("Кількість <p>:", paragraphs.length);
+
+const headings2 = document.querySelectorAll("h2");
+console.log("Кількість <h2>:", headings2.length);
+
+const bodyStyles = window.getComputedStyle(document.body);
+console.log("Фон <body>:", bodyStyles.backgroundColor);
+
+const h1 = document.querySelector("h1");
+if (h1) {
+  const h1Styles = window.getComputedStyle(h1);
+  console.log("Розмір шрифту <h1>:", h1Styles.fontSize);
+} else {
+  console.log("<h1> на сторінці немає");
+}
