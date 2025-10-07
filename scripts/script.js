6ff3991fc5e66b21ba34e9ecbc3a278ee59dbdc6
northwww <egorsavin943@gmail.com>
2025-10-07 21:36:51 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -13,21 +13,18 @@ function loadNewImages() {
   const productGrid = document.querySelector('.product-grid');
 
   imagesUrl.forEach((url, index) => {
-    setTimeout(() => { // ось тут ми обгортаємо створення елементів у setTimeout
-      const newCard = document.createElement('div');
-      newCard.classList.add('product-card');
-
+    setTimeout(() => {
+      // СОЗДАЕМ FRAGMENT
+      const fragment = document.createDocumentFragment();
       const img = document.createElement('img');
       img.src = url;
-      img.alt = `Додатковий продукт ${index + 1}`;
-
-      const title = document.createElement('h3');
-      title.textContent = `природа ${index + 1}`;
-
-      newCard.appendChild(img);
-      newCard.appendChild(title);
-
-      productGrid.appendChild(newCard);
-    }, index * 1000); // затримка 1 сек для кожного наступного
+      img.alt = `Зображення ${index + 1}`;
+      img.style.width = '200px';
+      img.style.margin = '10px';
+      
+      // Добавляем в fragment, потом fragment в DOM
+      fragment.appendChild(img);
+      productGrid.appendChild(fragment);
+    }, index * 1000);
   });
 }
