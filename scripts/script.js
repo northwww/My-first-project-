995a1a1f5905dd987761ca937aef8e8226a9ae67
northwww <egorsavin943@gmail.com>
2025-10-07 21:27:21 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,33 @@
-const paragraphs = document.querySelectorAll("p");
-console.log("Кількість <p>:", paragraphs.length);
-
-const headings2 = document.querySelectorAll("h2");
-console.log("Кількість <h2>:", headings2.length);
-
-const bodyStyles = window.getComputedStyle(document.body);
-console.log("Фон <body>:", bodyStyles.backgroundColor);
-
-const h1 = document.querySelector("h1");
-if (h1) {
-  const h1Styles = window.getComputedStyle(h1);
-  console.log("Розмір шрифту <h1>:", h1Styles.fontSize);
-} else {
-  console.log("<h1> на сторінці немає");
-}
-
-let elems = document.querySelectorAll("*"); 
-
-elems.forEach(elem => {
-    let oldColor = window.getComputedStyle(elem).backgroundColor;
-
-    elem.addEventListener("mouseenter", function() {
-        oldColor = window.getComputedStyle(elem).backgroundColor; 
-        elem.style.backgroundColor = "red";
-    });
-
-    elem.addEventListener("mouseleave", function() {
-        elem.style.backgroundColor = oldColor;
-    });
+window.addEventListener('load', () => {
+  setTimeout(loadNewImages, 5000);
 });
 
+function loadNewImages() {
+  const imagesUrl = [
+    "https://img.freepik.com/free-photo/mountains-lake_1398-1150.jpg?semt=ais_hybrid&w=740&q=80",
+    "https://img.freepik.com/free-photo/landscape-with-mountains-lake_1398-2287.jpg?semt=ais_hybrid&w=740&q=80",
+    "https://img.freepik.com/free-photo/landscape-with-mountains-lake_1398-1035.jpg?w=360",
+    "https://img.freepik.com/free-photo/mountains-lake_1398-1153.jpg?semt=ais_hybrid&w=740&q=80"
+  ];
+
+  const productGrid = document.querySelector('.product-grid');
+
+  imagesUrl.forEach((url, index) => {
+    setTimeout(() => { // ось тут ми обгортаємо створення елементів у setTimeout
+      const newCard = document.createElement('div');
+      newCard.classList.add('product-card');
+
+      const img = document.createElement('img');
+      img.src = url;
+      img.alt = `Додатковий продукт ${index + 1}`;
+
+      const title = document.createElement('h3');
+      title.textContent = `природа ${index + 1}`;
+
+      newCard.appendChild(img);
+      newCard.appendChild(title);
+
+      productGrid.appendChild(newCard);
+    }, index * 1000); // затримка 1 сек для кожного наступного
+  });
+}
